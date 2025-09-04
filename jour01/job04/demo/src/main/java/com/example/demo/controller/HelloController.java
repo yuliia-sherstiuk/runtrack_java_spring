a3ac0c603e82b93ebc9810bf0fe4bc511a798899
yuliia-sherstiuk <yuliia.sherstiuk@laplateforme.io>
2025-09-04 12:17:14 +0200

    Adding job04 for day 01

--- jour01/job04/demo/src/main/java/com/example/demo/controller/HelloController.java
+++ jour01/job04/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,7 @@
 
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
+    @Value("${greeting.message}")
+    private String message;
+
     @GetMapping("/")
     @ResponseBody
     public String hello() {
-        return "Hello World";
+        return message;
     }
 }
